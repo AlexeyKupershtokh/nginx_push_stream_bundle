ec51be6d08a8843d092b57fa67e3cc9513d9eea2
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2013-11-27 06:56:53 +0700

    Pub action

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Alawar\NginxPushStreamBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
@@ -10,5 +11,11 @@ class DefaultController extends Controller
     public function getSubUrlsAction($tokens)
     {
         return new Response(json_encode($tokens));
+    }
+
+    public function pubAction(Request $request)
+    {
+
+        return new Response(json_encode('ok'));
     }
 }
