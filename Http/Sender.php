f108f5aeebaa405de668cf54d79aa392d5e3817c
Alexey Kupershtokh <alexey.kupershtokh@gmail.com>
2014-01-30 15:45:34 +0700

    Sender tests

--- Http/Sender.php
+++ Http/Sender.php
@@ -3,13 +3,37 @@
 namespace Alawar\NginxPushStreamBundle\Http;
 
 use Guzzle\Http\Client;
+use Guzzle\Http\ClientInterface;
 
 class Sender implements SenderInterface
 {
+    /**
+     * @var \Guzzle\Http\Client|null
+     */
+    protected $client = null;
+
+    public function __construct()
+    {
+        $this->client = new Client();
+    }
+
+    /**
+     * @param ClientInterface $client
+     */
+    public function setClient(ClientInterface $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @param $url string
+     * @param $body string
+     * @param $headers array
+     * @return bool
+     */
     public function send($url, $body, $headers)
     {
-        $client = new Client($url);
-        $request = $client->post(null, $headers, $body);
+        $request = $this->client->post($url, $headers, $body);
         $response = $request->send();
         return $response->isSuccessful();
     }
--- Tests/Sender/SenderTest.php
+++ Tests/Sender/SenderTest.php
@@ -3,6 +3,9 @@
 namespace Alawar\NginxPushStreamBundle\Tests\Sender;
 
 use Alawar\NginxPushStreamBundle\Http\Sender;
+use Guzzle\Http\Client;
+use Guzzle\Http\Message\Request;
+use Guzzle\Http\Message\Response;
 
 class SenderTest extends \PHPUnit_Framework_TestCase
 {
@@ -12,4 +15,28 @@ class SenderTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf("Alawar\\NginxPushStreamBundle\\Http\\SenderInterface", $sender);
     }
 
+    public function testSend()
+    {
+        $url = 'http://localhost/pub?id=123';
+        $body = '{"token":"123"}' . "\r\n";
+        $headers = array(
+            'Event-ID'     => '123',
+            'Event-Type'   => 'new_message',
+            'Content-Type' => 'application/json'
+        );
+
+        $mockResponse = $this->getMock('Guzzle\Http\Message\Response', array(), array('200'));
+        $mockResponse->expects($this->once())->method('isSuccessful')->will($this->returnValue(true));
+
+        $mockRequest = $this->getMock('Guzzle\Http\Message\Request', array(), array('post', $url));
+        $mockRequest->expects($this->once())->method('send')->will($this->returnValue($mockResponse));
+
+        $mock = $this->getMock('Guzzle\Http\Client');
+        $mock->expects($this->once())->method('post')->with($url, $headers, $body)->will($this->returnValue($mockRequest));
+
+        $sender = new Sender();
+        $sender->setClient($mock);
+        $result = $sender->send($url, $body, $headers);
+        $this->assertEquals(true, $result);
+    }
 }
